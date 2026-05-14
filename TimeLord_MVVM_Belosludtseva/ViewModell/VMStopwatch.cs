5bdb6280478f1713a330172fcfdaf7a9fcd5e52c
yankabelosludtseva <nblsldcv@gmail.com>
2026-05-14 09:19:17 +0500

    Реализованное представление модели таймера. Белослудцева

---
+++ TimeLord_MVVM_Belosludtseva/ViewModell/VMStopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Threading;
+using Stopwatch = TimeLord_MVVM_Belosludtseva.Modell.Stopwatch;
+
+namespace TimeLord_MVVM_Belosludtseva.ViewModel
+{
+    public class VMStopwatch : INotifyPropertyChanged
+    {
+        public Stopwatch Stopwatch { get; set; }
+        private DispatcherTimer Timer = new DispatcherTimer()
+        {
+            Interval = new System.TimeSpan(0, 0, 1)
+        };
+
+        public VMStopwatch()
+        {
+            Stopwatch = new Stopwatch() { Work = false, Time = 0 };
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, System.EventArgs e)
+        {
+            if (Stopwatch.Work)
+                Stopwatch.Time++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
